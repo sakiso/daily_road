48c629292b3553dc8e13212ae9ccea036725a57a
sakiso <aki4s.dev@gmail.com>
2023-02-02 23:32:13 +0900

    トークン永続化

--- src/lib/stores/spotify_authorization_store.ts
+++ src/lib/stores/spotify_authorization_store.ts
@@ -1,9 +1,8 @@
+import { persist, createLocalStorage } from '@macfja/svelte-persistent-store';
 import { writable } from 'svelte/store';
 
-export const state = writable("");
-export const accessToken = writable("");
-// todo: 永続化
+export const state = writable('');
+export const accessToken = persist(writable(''), createLocalStorage(), 'accessToken'); // 永続化
 // todo: 認証前stateを設定できるようにする
 // todo: 認証後トークンを設定できるようにする
 //       読み書きできるようにする
-
