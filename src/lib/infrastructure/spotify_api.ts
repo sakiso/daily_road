9af447a2927928dedfc7db0a67ef661d8f2dc7e1
sakiso <aki4s.dev@gmail.com>
2023-02-06 22:00:19 +0900

    プロフィール取得機能仕掛中

--- src/lib/infrastructure/spotify_api.ts
+++ src/lib/infrastructure/spotify_api.ts
@@ -1,46 +1,44 @@
 import { state } from '$lib/stores/spotify_authorization_store';
+import axios from 'axios';
 
-export class SpotifyApi{
-  baseUrl: string;
+export class SpotifyApi {
+	baseUrl: string;
 
-  constructor(){
-    this.baseUrl = 'https://accounts.spotify.com';
-  }
+	constructor() {
+		this.baseUrl = 'https://accounts.spotify.com';
+	}
 
-  async login(clientId: string) {
-    const authorizePath = '/authorize'
-    const stateStr = Math.random().toString(36).slice(-5);  
-    const params = {
-      response_type: 'code',
-      client_id: clientId,
-      state: stateStr, //todo: unsubscribe必要？
-      scope: 'streaming user-library-read',
-      redirect_uri: 'http://localhost:5173/authorized'
-      //todo: 本番移行したときドメインが自動で変わるようにする
-    };
-  
-    state.set(stateStr); // note: 認証リダイレクト後に同一ユーザか判定するためstoreにstateを保存しておく
-    const queryParams = new URLSearchParams(params).toString();
-    window.location.href = `${this.baseUrl}${authorizePath}?${queryParams}`;
-  }
-  
-  async getCurrentUserProfile() {
-    // const spotifyAuthorizeBaseUrl = 'https://accounts.spotify.com/authorize?';
-    // const stateStr = Math.random().toString(36).slice(-5);
-  
-    // //todo: リダイレクト後にstateが一致しない場合はエラーにする
-    // const params = {
-    //   response_type: 'code',
-    //   client_id: client_id,
-    //   state: stateStr, //todo: unsubscribe必要？
-    //   scope: 'streaming user-library-read',
-    //   redirect_uri: 'http://localhost:5173/authorized'
-    //   //todo: 本番移行したときドメインが自動で変わるようにする
-    // };
-  
-    // // note: 認証リダイレクト後に本当に同一ユーザか判定するためstoreにstateを保存しておく
-    // state.set(stateStr);
-    // const queryParams = new URLSearchParams(params).toString();
-    // window.location.href = `${spotifyAuthorizeBaseUrl}${queryParams}`;
-  }
+	async login(clientId: string) {
+		const authorizePath = '/authorize';
+		const stateStr = Math.random().toString(36).slice(-5);
+		const params = {
+			response_type: 'code',
+			client_id: clientId,
+			state: stateStr, //todo: unsubscribe必要？
+			scope: 'streaming user-library-read',
+			redirect_uri: 'http://localhost:5173/authorized'
+			//todo: 本番移行したときドメインが自動で変わるようにする
+		};
+
+		state.set(stateStr); // note: 認証リダイレクト後に同一ユーザか判定するためstoreにstateを保存しておく
+		const queryParams = new URLSearchParams(params).toString();
+		window.location.href = `${this.baseUrl}${authorizePath}?${queryParams}`;
+	}
+
+	async getCurrentUserProfile(accessToken: string): Promise<object> {
+    // todo: CORS出る！！ proxyかます
+    // https://stackoverflow.com/questions/72753092/how-to-proxy-on-svelte-kit-in-dev-mode が参考になりそう
+		const currentUserProfilePath = '/me';
+		let response = {};
+		axios
+			.get(`${this.baseUrl}${currentUserProfilePath}`, {
+				headers: {
+					Authorization: `Bearer ${accessToken}`
+				}
+			})
+			.then((res) => {
+				response = res;
+			});
+		return response;
+	}
 }
--- src/routes/play_music/+page.svelte
+++ src/routes/play_music/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import { SpotifyApi } from '$lib/infrastructure/spotify_api';
 	import { accessToken } from '$lib/stores/spotify_authorization_store';
 	import Button, { Label } from '@smui/button';
 
-	function playback() {
-		// todo: 切り出したい
+	let myProfile = {};
+
+	function playback(): void {
 		console.log('playback!');
 		console.log($accessToken);
 		//todo: ここでSpotifyAPIに接続して再生する
+	}
+	function getCurrentUserProfile(): void {
+		const spotifyApi = new SpotifyApi();
+		myProfile = spotifyApi.getCurrentUserProfile($accessToken);
 	}
 </script>
 
@@ -21,6 +27,14 @@
 		<Button on:click={playback} variant="raised">
 			<Label>Playback!</Label>
 		</Button>
+		<br />
+		<br />
+		<Button on:click={getCurrentUserProfile} variant="raised">
+			<Label>getMyProfile!</Label>
+		</Button>
+	</div>
+	<div>
+		<p />
 	</div>
 </main>
 
