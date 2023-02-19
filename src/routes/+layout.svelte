321fee04ca887f1b6b246c2b561ada968a42e19c
sakiso <aki4s.dev@gmail.com>
2023-02-19 19:33:01 +0900

    バニラなPlayerページ作成

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	<nav>
 		<a href="/play_music">play music</a>
 		<a href="/">home</a>
+		<a href="/player">player</a>
 	</nav>
 	<!-- NOTE: slotに+pageが入る -->
 	<slot />
---
+++ src/routes/player/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>player</title>
+</svelte:head>
+
+<main>
+	<h1>player</h1>
+</main>
+
+<style>
+</style>
