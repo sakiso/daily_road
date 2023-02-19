695138fa80c5fc4cad12bb145a05af8d93830514
sakiso <aki4s.dev@gmail.com>
2023-02-19 11:35:25 +0900

    トークン取得成功

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export default class Constants {
+  static spotifyAuthorizeUrl = 'https://accounts.spotify.com/authorize'; // todo: ちゃんとドメインだけ共通化する
+  static spotifyGetTokenUrl = 'https://accounts.spotify.com/api/token'
+  static spotifyApiBaseUrl = 'https://api.spotify.com/v1'
+}
