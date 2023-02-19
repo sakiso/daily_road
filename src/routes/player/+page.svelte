9e39f6c1481ddd63e7850caede0ffa97477f27cc
sakiso <aki4s.dev@gmail.com>
2023-02-19 20:15:59 +0900

    死ぬほどエラー出てるけどスマホから再生できる

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -1,8 +1,49 @@
-<script lang="ts">
+<script>
+	import { accessToken } from '$lib/stores/spotify_authorization_store';
+
+	window.onSpotifyWebPlaybackSDKReady = () => {
+		const token = $accessToken;
+		console.log('🚀 ~ file: +page.svelte:25 ~  $accessToken', $accessToken);
+		const player = new Spotify.Player({
+			name: 'Web Playback SDK Quick Start Player',
+			getOAuthToken: (cb) => {
+				cb(token);
+			},
+			volume: 0.5
+		});
+		// Ready
+		player.addListener('ready', ({ device_id }) => {
+			console.log('Ready with Device ID', device_id);
+		});
+
+		// Not Ready
+		player.addListener('not_ready', ({ device_id }) => {
+			console.log('Device ID has gone offline', device_id);
+		});
+
+		// Other Utillity Listener
+		player.addListener('initialization_error', ({ message }) => {
+			console.error(message);
+		});
+
+		player.addListener('authentication_error', ({ message }) => {
+			console.error(message);
+		});
+
+		player.addListener('account_error', ({ message }) => {
+			console.error(message);
+		});
+		player.connect();
+	};
 </script>
 
 <svelte:head>
 	<title>player</title>
+	<body>
+		<h1>Spotify Web Playback SDK Quick Start</h1>
+		<button id="togglePlay">Toggle Play</button>
+		<script src="https://sdk.scdn.co/spotify-player.js"></script>
+	</body>
 </svelte:head>
 
 <main>
