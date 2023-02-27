c85d1438cd10e541e08aea1d54f1bd5d5773c37b
sakiso <aki4s.dev@gmail.com>
2023-02-27 22:13:49 +0900

    コメントなど整え ゲームプレイを切り出し準備

--- src/routes/player/game.svelte
+++ src/routes/player/game.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	const NOTE_WIDTH = 70;
+	const NOTE_HEIGHT = 5;
+	const LANE_COUNT = 4;
+	const LANE_SPACING = 5;
+	const NOTE_SPEED = 1;
+	const NOTE_HI_SPEED = 3;
+
+	const NOTES = [
+		//todo: ドメインモデル作ろう Speedは定数で管理しよう
+		{ lane: 0, position: 0 },
+		{ lane: 1, position: -10 },
+		{ lane: 2, position: -20 },
+		{ lane: 3, position: -30 },
+		{ lane: 1, position: -40 },
+		{ lane: 0, position: -50 },
+		{ lane: 3, position: -60 },
+		{ lane: 2, position: -70 }
+	];
+
 	let elapsedTimeFromGameStart = 0;
 	let key = '';
 	let latestKeyDownTimes = {
@@ -9,76 +28,53 @@
 		j: 0,
 		k: 0
 	};
+	let ctx: CanvasRenderingContext2D;
+	let canvas: HTMLCanvasElement;
 
 	onMount(() => {
-		const canvas = <HTMLCanvasElement>document.getElementById('game-canvas')!;
-		let ctx = canvas.getContext('2d')!;
-		// // Canvasをクリアする
-		// ctx.clearRect(0, 0, canvas.width, canvas.height);
-		// Canvasの背景色を薄いグレーに設定
-
-		// ctx.fillStyle = '#000000';
-
-		const NOTE_WIDTH = 70;
-		const NOTE_HEIGHT = 5; //canvas.height;
-		const LANE_COUNT = 4;
-		const LANE_SPACING = 5;
-		const NOTE_SPEED = 1;
-		const NOTE_HI_SPEED = 3;
-
-		const NOTES = [
-			//todo: ドメインモデル作ろう Speedは定数で管理しよう
-			{ lane: 0, position: 0 },
-			{ lane: 1, position: -10 },
-			{ lane: 2, position: -20 },
-			{ lane: 3, position: -30 },
-			{ lane: 1, position: -40 },
-			{ lane: 0, position: -50 },
-			{ lane: 3, position: -60 },
-			{ lane: 2, position: -70 }
-		];
-
-		function drawLanes() {
-			for (let i = 0; i < LANE_COUNT; i++) {
-				const x = i * (NOTE_WIDTH + LANE_SPACING);
-				const y = 0;
-				ctx.fillStyle = '#354f77';
-				ctx.fillRect(x, y, NOTE_WIDTH, canvas.height);
-				ctx.fillStyle = '#000'; // 初期化
-			}
-		}
-
-		function drawNotes() {
-			for (const note of NOTES) {
-				const x = note.lane * (NOTE_WIDTH + LANE_SPACING);
-				const y = note.position * NOTE_HI_SPEED;
-				ctx.fillStyle = '#70e5ba';
-				ctx.fillRect(x, y, NOTE_WIDTH, NOTE_HEIGHT);
-			}
-		}
-
-		function updateNotes() {
-			for (const note of NOTES) {
-				note.position += NOTE_SPEED;
-			}
-		}
-
-		function gameLoop(timestamp?: DOMHighResTimeStamp) {
-			console.log(timestamp);
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			drawLanes();
-			drawNotes();
-			updateNotes();
-			timestamp ||= 0;
-			elapsedTimeFromGameStart = timestamp;
-			if (timestamp < 2000) {
-				// 開発用に一定時間で止める
-				requestAnimationFrame(gameLoop);
-			}
-		}
-
-		gameLoop();
+		canvas = <HTMLCanvasElement>document.getElementById('game-canvas')!;
+		ctx = canvas.getContext('2d')!;
+		// todo: リスナーを張って上のコンポーネントで楽曲再生されたら同時にゲーム画面をスタートさせる
 	});
+
+	function drawLanes() {
+		for (let i = 0; i < LANE_COUNT; i++) {
+			const x = i * (NOTE_WIDTH + LANE_SPACING);
+			const y = 0;
+			ctx.fillStyle = '#354f77';
+			ctx.fillRect(x, y, NOTE_WIDTH, canvas.height);
+			ctx.fillStyle = '#000'; // 初期化
+		}
+	}
+
+	function drawNotes() {
+		for (const note of NOTES) {
+			const x = note.lane * (NOTE_WIDTH + LANE_SPACING);
+			const y = note.position * NOTE_HI_SPEED;
+			ctx.fillStyle = '#70e5ba';
+			ctx.fillRect(x, y, NOTE_WIDTH, NOTE_HEIGHT);
+		}
+	}
+
+	function updateNotes() {
+		for (const note of NOTES) {
+			note.position += NOTE_SPEED;
+		}
+	}
+
+	function gameLoop(timestamp?: DOMHighResTimeStamp) {
+		console.log(timestamp);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawLanes();
+		drawNotes();
+		updateNotes();
+		timestamp ||= 0;
+		elapsedTimeFromGameStart = timestamp;
+		if (timestamp < 2000) {
+			// 開発用に一定時間で止める
+			requestAnimationFrame(gameLoop);
+		}
+	}
 
 	function handleKeydown(event: KeyboardEvent) {
 		key = event.key; // todo: これが押されたときのelapsedTimeFromGameStartを取得する
